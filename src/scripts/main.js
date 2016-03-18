bc09b3263b3be3a736772799b7e8ce8f19ade3eb
DANIEL A. ST CLAIR <danielstclair@us.ibm.com>
2016-03-18 16:09:32 -0500

    finished building app in jquery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,31 +5,68 @@ import $ from 'jquery';
 import YTSearch from 'youtube-api-search';
 import API_KEY from './api_key';
 
-var SearchBar = '<form class="search-bar"><input placeholder="Let\'s search the youtube verse" /></form>';
-var VideoDetail = '';
+var state = {
+  "term": '',
+  "videos": [],
+  "videosHTML": '',
+  "selectedVideo": {},
+  "selectedVideoHTML": ''
+};
 
-function searchVids (term) {
-  YTSearch({key: API_KEY, term: term}, function(videos){
-    console.log('videos ' , videos);
-    var videoUrl = 'https://www.youtube.com/embed/' + videos[0].id.videoId;
-    VideoDetail =  '<div class="video-detail col-md-8"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" allowfullscreen="allowfullscreen" src=' + videoUrl + '></iframe></div><div class="details"><h6>' + videos[0].snippet.title + '</h6><span>' + videos[0].snippet.description +'</span></div></div>';
-    render(VideoDetail);
+function initRender(){
+  var SearchBar = '<form class="search-bar"><input placeholder="Let\'s search the youtube verse" /></form>';
+  var App = '<section>' + SearchBar + '</section><section id="video-section"></section>';
+  $('#container').html(App);
+  $('.search-bar').on('submit', searchVids);
+}
+
+function searchVids(e){
+  e.preventDefault();
+  state.term = e.target.children[0].value;
+  getVids(state.term);
+}
+
+function getVids(term) {
+  var params = {
+    part: 'snippet',
+    key: API_KEY,
+    q: term,
+    type: 'video'
+  };
+  $.get('https://www.googleapis.com/youtube/v3/search', params)
+    .done(function(data){
+      state.videos = data.items.slice();
+      vidsList(state.videos)
+    });
+}
+
+function vidsList(videos){
+  var VideoListItems = videos.map(function(video, i){
+    return '<li class="list-group-item"><div class="video-list media"><div class="media-left"><img class="media-object" src=' + video.snippet.thumbnails.default.url + ' /></div></div><div class="media-body"><div class="media-heading">' + video.snippet.title + '</div></div></li>';
+  });
+  var VideoList = '<ul class="col-md-4 list-group">' + VideoListItems.join('') + '</ul>';
+  state.selectedVideo = videos[0];
+  state.videosHTML = VideoList;
+  vidsPlayer(0);
+}
+
+function vidsPlayer(num) {
+  var videoUrl = 'https://www.youtube.com/embed/' + state.videos[num].id.videoId;
+  var VideoDetail =  '<div class="video-detail col-md-8"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" allowfullscreen="allowfullscreen" src=' + videoUrl + '></iframe></div><div class="details"><h6>' + state.videos[num].snippet.title + '</h6><span>' + state.videos[num].snippet.description +'</span></div></div>';
+  state.selectedVideo = state.videos[num];
+  state.selectedVideoHTML = VideoDetail;
+  vidsRender();
+}
+
+function vidsRender(VideoDetail, VideoList) {
+  VideoDetail = state.selectedVideoHTML;
+  VideoList = state.videosHTML;
+  var VideoSection = VideoDetail + VideoList;
+  $('#video-section').html(VideoSection);
+  var listItems = [].slice.call(document.querySelectorAll('.list-group-item'));
+  listItems.map(function(item, i){
+    return item.addEventListener('click', function(){ vidsPlayer(i)});
   });
 }
 
-function changeSearch(e){
-  e.preventDefault();
-  console.log(e.target.children[0].value);
-  searchVids(e.target.children[0].value);
-}
-
-
-
-function render(video){
-  var App = video !== '' ? '<div>' + SearchBar + VideoDetail +'</div>' : '<div>' + SearchBar + '</div>';
-  $('#container').html(App);
-  $('.search-bar').on('submit', changeSearch);
-}
-
-// searchVids('surfboards')
-render();
+initRender();
